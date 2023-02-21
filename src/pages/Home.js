597147c028a2d10e94bbae13bf50e207ca1ab5d7
Haritha Weerathunga <122088594+harithaw@users.noreply.github.com>
2023-02-21 07:29:11 +0530

    react-app-initilised

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Home = () => {
+	const navigate = useNavigate();
+	return (<h1>Home</h1>);
+};
+
+export default Home;
