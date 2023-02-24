9b84012629760877e86eb5d1af1c9d9424cf15c8
215131E <112235122+215131E@users.noreply.github.com>
2023-02-24 02:51:47 -0800

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import './css/Home.css'
 
 const Home = () => {
 	const navigate = useNavigate();
 	return (
-		<div className="mainDiv">
-        <div className="headDiv">Sign In</div>
-        <div className="inputDiv"><input type="text" className="inputFields" /></div>
-        <div className="inputDiv"><input type="text" className="inputFields" /></div>
-        <div className="btnDiv"><button>Sign In</button></div>
+		<div className="bookform">
+        <div className="mainDiv">
+          <div className="headDiv">Sign In</div>
+          <div className="inputDiv"><input type="text" className="inputFields" /></div>
+          <div className="inputDiv"><input type="text" className="inputFields" /></div>
+          <div className="btnDiv"><button>Sign In</button></div>
+        </div>
       </div>
 	);
 };
